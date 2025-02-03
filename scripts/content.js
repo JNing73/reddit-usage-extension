61dd29d3ea635e333a935a248407ec756636158f
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-02-03 16:49:55 +1300

    Update proof of concept timer to read and write from localstorage

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,29 @@
 console.log("Content Script Triggered Correctly");
 
-let totalTime = localStorage.getItem("totalRedditTime") ?? 0;
+const totalTimeKey = "totalRedditTime";
+
+active = true;
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function getStoredTotalTime() {
+    storedTime = localStorage.getItem(totalTimeKey) ?? 0;
+    if (storedTime == 0) {
+        localStorage.setItem(totalTimeKey, storedTime);
+    }
+    return storedTime;
+}
+
 async function startActiveTracking() {
 
-    instaceTime = 0;
-
-    while (true) {
-        await delay(1000);
-        instaceTime++;
-        console.log(instaceTime);
+    while (active) {
+        await delay(5000); // Count in 5-second increments
+        let totalTime = Number(getStoredTotalTime());
+        totalTime += 5;
+        localStorage.setItem(totalTimeKey, totalTime);
+        console.log(totalTime);
     }
 
 }
