10499dfc50d1b52598b94419892110a4b3fcd58b
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-02-03 16:15:23 +1300

    Implement a proof of concept for a timer that starts counting when trigger site is loaded

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,21 @@
 console.log("Content Script Triggered Correctly");
+
+let totalTime = localStorage.getItem("totalRedditTime") ?? 0;
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function startActiveTracking() {
+
+    instaceTime = 0;
+
+    while (true) {
+        await delay(1000);
+        instaceTime++;
+        console.log(instaceTime);
+    }
+
+}
+
+startActiveTracking();
