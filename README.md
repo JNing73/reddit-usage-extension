38654558a5e0b12c6c5cb453fc1db985629956b0
JNing73 <94809226+JNing73@users.noreply.github.com>
2025-02-03 18:09:56 +1300

    Update proof of concept to pause/resume the timer based on whether the tab is active or not

--- README.md
+++ README.md
@@ -7,7 +7,15 @@
 - Toggle 'Developer mode' to on
 - Click on 'Load unpacked'
 - Select the directory containing the repo on your local machine
-- You should now be able to open the extension through the extension widget 
+- You should now be able to open the extension through the extension widget
+
+## Notable behavioural points
+- The time counter only runs while the current tab is active
+  - Have accounted for edge cases where a user may rapidly switch to and from the active tab
+
+## Key concepts demonstrated
+- Writing and utilising asynchronous functions in javascript
+- Gracefully managing a function to only run its parent page is active
 
 ### Additional Notes:
 Icon file generated through iOS image playground
--- scripts/content.js
+++ scripts/content.js
@@ -2,30 +2,48 @@ console.log("Content Script Triggered Correctly");
 
 const totalTimeKey = "totalRedditTime";
 
-active = true;
+localStorage.clear();
+
+let active = true;
+let oneActiveInstace = false;
 
 function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function getStoredTotalTime() {
-    storedTime = localStorage.getItem(totalTimeKey) ?? 0;
-    if (storedTime == 0) {
-        localStorage.setItem(totalTimeKey, storedTime);
-    }
-    return storedTime;
+  storedTime = localStorage.getItem(totalTimeKey) ?? 0;
+  // initialise local storage value-pair for this app
+  if (storedTime == 0) {
+    localStorage.setItem(totalTimeKey, storedTime);
+  }
+  return storedTime;
 }
 
 async function startActiveTracking() {
 
-    while (active) {
+  if (oneActiveInstace) {
+    console.log("Previous instance hadn't completed yet, will just resume that")
+    return;
+  }
+  console.log("new instance of counter started");
+  while (active) {
+        oneActiveInstace = true;
         await delay(5000); // Count in 5-second increments
         let totalTime = Number(getStoredTotalTime());
         totalTime += 5;
         localStorage.setItem(totalTimeKey, totalTime);
         console.log(totalTime);
     }
-
+  oneActiveInstace = false;
+  console.log("Current instance has finished")
 }
+
+document.addEventListener("visibilitychange", function () {
+    active = !active;
+    if (active) {
+        startActiveTracking();
+    }
+});
 
 startActiveTracking();
